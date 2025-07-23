d4a0ac632e85dea6871042198dcf9d4017825803
Dominick Founds <dom@solace.health>
2025-07-23 19:49:54 -0400

    player pickups done

--- Assets/Scripts/area_exit.gd
+++ Assets/Scripts/area_exit.gd
@@ -1,8 +1,25 @@
 extends Area2D
 
+class_name AreaExit
+
+@export var sprite : Sprite2D
+
+var is_open = false
+
+func _ready():
+	# closing the portal
+	close()
+
+func open():
+	# do stufffff
+	is_open = true
+	sprite.region_rect.position.x = 22
+
+func close():
+	is_open = false
+	sprite.region_rect.position.x = 0
 
 func _on_body_entered(body: Node2D) -> void:
-	if body is PlayerController:
+	if is_open && body is PlayerController:
 		#do stuff
 		GameManager.next_level()
-	
